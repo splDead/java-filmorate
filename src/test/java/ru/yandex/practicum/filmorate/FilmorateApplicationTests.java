b9cdcc39657dc524468884fe898e30fd23f252c4
Sergey Goryachev <spld@yandex.ru>
2026-07-27 17:21:00 +0300

    sprint 12, final task

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @JdbcTest
 @AutoConfigureTestDatabase
 @ComponentScan(basePackages = "ru.yandex.practicum.filmorate")
-class FilmoRateApplicationTests {
+class FilmorateApplicationTests {
 	@Autowired
 	@Qualifier("userDbStorage")
 	private UserStorage userStorage;
